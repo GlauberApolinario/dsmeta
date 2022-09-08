12afc0e66a0ea0ecd765e0db38420bc7ef125f1c
Glauber Apolinario de Freitas <glauber.apolinario@yahoo.com.br>
2022-09-07 23:32:00 -0300

    Other Components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,19 @@
-import NotificationButton from "./components/NotificationButton"
+import Header from "./components/Header";
+import './index.css'
+import SalesCard from "./components/SalesCard";
 
 function App() {
-  return (
-    <>
-    <h1>Olá Mundo!</h1>
-    <NotificationButton/>
-    </>
-  )
+	return (
+		<>
+			<Header />
+			<main>
+				<section id="sales">
+					<div className="dsmeta-container"></div>
+          <SalesCard/>
+				</section>
+			</main>
+		</>
+	);
 }
 
-export default App
+export default App;
